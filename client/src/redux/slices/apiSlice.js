ecdc138fa5061386923b4f067bc27b91342694d2
Alok Raj <alokr8950@gmail.com>
2026-05-07 17:05:49 +0530

    connect render backend

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const API_URL =
   import.meta.env.VITE_APP_BASE_URL ||
-  "https://taskify-production-5f8e.up.railway.app";
+  "https://taskify-dqw8.onrender.com";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: API_URL,
